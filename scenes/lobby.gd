47821b965ff865dc41aa50bee3db5040337eaaad
greenerino <greenejustint@gmail.com>
2023-07-31 19:53:28 -0700

    Different way to swap scenes

--- scenes/lobby.gd
+++ scenes/lobby.gd
@@ -6,23 +6,27 @@ var game_scene = preload("res://scenes/game.tscn")
 @export var main_menu: Control
 @export var new_game_menu: Control
 @onready var menus = [main_menu, new_game_menu]
+var game: Game = null
 
-func swap_menus(menu: Control):
+func swap_menus(menu: Node):
 	for m in menus:
 		m.visible = false
+
+	if not menus.has(menu):
+		self.visible = false
+	else:
+		self.visible = true
+	
 	if menu:
 		menu.visible = true
-
-func swap_scenes(scene: Node):
-	get_tree().root.add_child(scene)
-	get_parent().remove_child(self)
 
 func _on_main_menu_new_game_requested():
 	swap_menus(new_game_menu)
 
 func _on_new_game_menu_create_game(base_minutes, increment, side):
-	var game: Game = game_scene.instantiate()
+	game = game_scene.instantiate()
 	game.base_minutes = base_minutes
 	game.increment = increment
 	game.perspective = side
-	swap_scenes(game)
+	add_sibling(game)
+	swap_menus(game)
